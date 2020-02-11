e14bad495d42de7f0e1a83aa1ad5480542a2a0e1
KilianCorbel <kilian-corbel@live.fr>
2020-02-11 16:55:35 +0100

    page compte, resp

--- wito_front/src/Components/ModalMain/MonCompte.js
+++ wito_front/src/Components/ModalMain/MonCompte.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 import CheckAuth from '../Main/CheckAuth';
 import Grid from '@material-ui/core/Grid';
-import Link from '@material-ui/core/Link';
+import Avatar from '@material-ui/core/Avatar';
 import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
-import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
-import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import { withRouter } from 'react-router';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+import TextField from '@material-ui/core/TextField';
 import { withStyles } from '@material-ui/styles';
 
 
@@ -19,8 +26,7 @@ const styles = theme => ({
       //smargin: theme.spacing(5),
     },
     card: {
-        width: '100%',
-        marginBottom: 10,
+        margin: '0 auto',
     },
     cardCategoryWhite: {
         color: "rgba(255,255,255,.62)",
@@ -37,16 +43,25 @@ const styles = theme => ({
         fontFamily: "'Roboto', 'Helvetica', 'Arial', sans-serif",
         marginBottom: "3px",
         textDecoration: "none"
+      },
+      avatar: {
+        width: '10vh',
+        height: '10vh',
+      },
+      top: {
+        marginTop: '8vh',
       }
 });
 
 class MonCompte extends Component {
   constructor(props){
       super(props);
+      this.handleSubmit = this.handleSubmit.bind(this);
       this.state = {
           value: '',
           setValue: '',
-          student: null,
+          user: '',
+          open : false,
       }
   }
 
@@ -55,9 +70,9 @@ class MonCompte extends Component {
 
       fetch(window.location.protocol + '//' + window.location.hostname + ':3010/utilisateurs/' + localStorage.getItem("user_id"))
       .then((resp) => resp.json())
-      .then(function(student) {
-          console.log(student);
-          currentComponent.setState(student);
+      .then(function(user) {
+          console.log(user);
+          currentComponent.setState(user);
       })
   }
 
@@ -69,52 +84,135 @@ class MonCompte extends Component {
     this.setState({setValue : index});
   };
 
+  handleClickOpen = () => {
+    this.setState({open : true});
+  };
+  
+  handleClose = () => {
+    this.setState({open : false});
+    window.location.reload();
+  };
+
+  handleSubmit(event) {
+    let currentComponent = this;
+    let mdp = document.getElementById('mdp').value;
+    let confmdp = document.getElementById('confmdp').value;
+
+    console.log(mdp);
+    console.log(confmdp);
+    if (mdp === confmdp) {
+      fetch(window.location.protocol + '//' + window.location.hostname + ':3010/utilisateurs/' + localStorage.getItem('user_id'),{
+          method: 'PUT',
+          body: JSON.stringify({
+            mdp: mdp,
+        })
+      });
+      this.handleClose();
+    }
+  }
+
   render() {
     const {classes} = this.props;
+    const user = this.state;
 
     return (
         <div className={classes.root}>
             <CheckAuth/>
 
-            <Grid spacing={3}>
-                <Grid item md={4}></Grid>
-
-                <Grid item xs={12} md={4}>
-
+            <Grid spacing={3} className={classes.top}>
+                <Grid item md={3}></Grid>
+                  
+                <Grid container xs={12} md={6}>
+                  <Grid item xs={2}></Grid>
+                  <Grid item xs={8}>
+                  <Card className={classes.card} >
+                        <CardContent>
+                          <Grid container spacing={3}>
+                            <Grid item xs={4}>
+                              <Avatar alt="" className={classes.avatar} src="./src\account.png" />
+                            </Grid>
+                            <Grid item xs={6}>
+                              <Typography gutterBottom className={classes.typo} variant="h5" component="h2">
+                                {user.nom} {user.prenom}
+                              </Typography>
+                              <Typography variant="body2" color="textSecondary" className={classes.typo} component="p">
+                                {user.mail} 
+                                </Typography>
+                              </Grid>
+                            <Grid item xs={3}>
+                              
+                            </Grid>
+                          </Grid>
+                          <Grid container spacing={3}>
+                            <Grid item xs={4}>
+                              
+                            </Grid>
+                            <Grid item xs={8}>
+                            <Button variant="contained" onClick={this.handleClickOpen} color="secondary">
+                                Modifier le mot de passe
+                              </Button>
+                            </Grid>
+                          </Grid>
+                        </CardContent>
+                    </Card>
+                  </Grid>
                 </Grid>
 
-                <Grid item md={4}></Grid>
+                <Grid item md={3}></Grid>
             </Grid>
 
-            <Grid spacing={3}>
-                <Grid item md={4}></Grid>
-
-                <Grid item xs={12} md={4}>
-
-                </Grid>
-
-                <Grid item md={4}></Grid>
-            </Grid>
-
-            <Grid spacing={3}>
-                <Grid item md={4}></Grid>
-
-                <Grid item xs={12} md={4}>
-
-                </Grid>
-
-                <Grid item md={4}></Grid>
-            </Grid>
-
-            <Grid spacing={3}>
-                <Grid item md={4}></Grid>
-
-                <Grid xs={12}  md={4}>
-
-                </Grid>
-
-                <Grid item md={4}></Grid>
-            </Grid>
+            <Dialog
+            fullWidth={this.state.fullWidth}
+            maxWidth={this.state.maxWidth}
+            open={this.state.open}
+            onClose={this.state.handleClose}
+            aria-labelledby="max-width-dialog-title"
+          >
+            
+          <form className={classes.form} noValidate>
+          <FormControl className={classes.formControl}>
+            <DialogTitle>Modifier le mot de passe</DialogTitle>
+            <DialogContent>
+              <DialogContentText>
+                  <Grid container spacing={3}>
+                    <Grid item xs={12} md={12}>
+                      <TextField
+                        autoFocus
+                        margin="dense"
+                        id="mdp"
+                        label="Nouveau mot de passe"
+                        type="mdp"
+                        fullWidth
+                      />
+                    </Grid>                  
+                    
+                                   
+                  </Grid>
+                  <Grid container spacing={3}>
+                    <Grid item xs={12} md={12}>
+                        <TextField
+                          
+                          margin="dense"
+                          id="confmdp"
+                          label="Confirmez le mot de passe"
+                          type="confmdp"
+                          fullWidth
+                        />
+                    </Grid>  
+                  </Grid>
+              </DialogContentText>
+            </DialogContent>
+            <DialogActions>
+              <Button onClick={this.handleSubmit} color="primary">
+                Modifier le mot de passe
+              </Button>
+              <Button onClick={this.handleClose} color="secondary">
+                Fermer
+              </Button>
+            </DialogActions>
+          </FormControl>
+          </form>
+          </Dialog>
         </div>
       );
   }
